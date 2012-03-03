b8418a8d8cff88a7baec044bfa95ef6288fff9f4
tatico <tatico@gmail.com>
2012-03-03 19:51:49 +0100

    basic idea

--- src/pydog.py
+++ src/pydog.py
@@ -1,8 +1,23 @@
 #!/usr/bin/env python
 
-import sys, time
+#pydog.py
 
-# check is file is present
-# if isfile(
-# open file to write or append
-#dogfile = open(
+import os, time, sys
+
+#sys.argv[1]
+if len(sys.argv) != 1:
+  dogfile = sys.argv[2]
+  ifile   = sys.argv[1] 
+  # check is file is present
+  if os.path.isfile(dogfile):
+    # open file to append
+    print()
+  else:
+    #open file to write
+    #dogfile = open(
+    print()
+  print("I'll do lots of things")
+
+else:
+  print("Usage:")
+  print("pydog.py <string|file> <file>")
